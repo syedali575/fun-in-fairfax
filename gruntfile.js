5bc0eac15e89a039284bc24511b537c028650457
Syed Ali <sali3@cox.net>
2016-12-20 13:41:36 -0500

    Got first state working

--- gruntfile.js
+++ gruntfile.js
@@ -63,8 +63,14 @@ module.exports = function(grunt){
             cwd: "node_modules/angular/",
             src: ["angular.js"],
             dest: "build/js"
-          }
+          },
+          {  expand: true,
+            cwd: "node_modules/angular-ui-router/release/",
+            src: ["angular-ui-router.js"],
+            dest: "build/js"
+          }  
         ]
+
       },
     // End of copy
     },
