d82e5d6c7da308789f388544c9183a86d072544c
Syed Ali <sali3@cox.net>
2016-12-21 09:57:32 -0500

    Able to retrive data on park view

--- build/js/app.js
+++ build/js/app.js
@@ -69,16 +69,24 @@
   function ParksController(ParkService){
     // console.log("in ParksController");
 
+    var vm = this;
+    vm.message = "";
+    vm.parkData = [];
 
     this.getParks = function getParks(){
       console.log("In getPark Function");
       ParkService.parkList()
       .then(function sucessHandeler(data){
         console.log("Its working", data);
-        console.log(data.data.searchResults.results[0].doc.metadata.label);
+        console.log(data.data.searchResults.results[5].doc.metadata.label);
+        vm.parkData = data.data.searchResults.results;
+
+        console.log("Getting my array", vm.parkData);
       })
       .catch(function failHandeler(xhr){
         console.log("Unable to communicate", xhr);
+        vm.message = "We are unable to communicate, please try again";
+
       });
     };
 
