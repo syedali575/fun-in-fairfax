e29db6f0e35888361d2719e4df26a20484d0a724
Syed Ali <sali3@cox.net>
2017-01-05 12:11:52 -0500

    fixed view issues by creating a custom object

--- build/js/app.js
+++ build/js/app.js
@@ -570,11 +570,37 @@
     };
 
     function addToPlan(data){
-      // var todaysPlan = [];
-      todaysPlan.push(data);
 
-      console.log("addToPlan is working", data);
-      console.log("Whats in todaysPlan", todaysPlan);
+      var updatedObject = {
+        name: null,
+        streetNumber: null,
+        address: null,
+        zip: null,
+        phone: null,
+        web: null
+      };
+
+      if (data.searchResults.PARKS_FCPA) {
+        updatedObject.name = data.searchResults.PARKS_FCPA.PARK_NAME;
+        updatedObject.address = data.searchResults.PARKS_FCPA.STREET;
+        updatedObject.zip = data.searchResults.PARKS_FCPA.ZIP_CODE;
+      }
+      else if (data.searchResults.Libraries) {
+        updatedObject.name = data.searchResults.Libraries.DESCRIPTION;
+        updatedObject.streetNumber = data.searchResults.Libraries.STREET_NUMBER;
+        updatedObject.address = data.searchResults.Libraries.STREET_NAME;
+        updatedObject.zip = data.searchResults.Libraries.ZIP;
+        updatedObject.phone = data.searchResults.Libraries.ERC_PHONE;
+        updatedObject.web = data.searchResults.Libraries.WEB_ADDRESS;
+      }
+
+
+
+
+      todaysPlan.push(updatedObject);
+
+      console.log("addToPlan is working", updatedObject);
+      console.log("Whats in todaysPlan", updatedObject);
       // localStorage.setItem("plan", angular.toJson(todaysPlan));
     }
 
