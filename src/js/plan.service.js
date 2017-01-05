018206bdd7a3823593f156f94ba8f57248064bea
Syed Ali <sali3@cox.net>
2017-01-05 13:43:42 -0500

    Able to show today's plan in proper format, and correct choices

--- src/js/plan.service.js
+++ src/js/plan.service.js
@@ -18,6 +18,7 @@
 
       var updatedObject = {
         name: null,
+        feature: null,
         streetNumber: null,
         address: null,
         zip: null,
@@ -27,16 +28,27 @@
 
       if (data.searchResults.PARKS_FCPA) {
         updatedObject.name = data.searchResults.PARKS_FCPA.PARK_NAME;
+        updatedObject.feature = "Fairfax County Park";
         updatedObject.address = data.searchResults.PARKS_FCPA.STREET;
         updatedObject.zip = data.searchResults.PARKS_FCPA.ZIP_CODE;
       }
       else if (data.searchResults.Libraries) {
         updatedObject.name = data.searchResults.Libraries.DESCRIPTION;
+        updatedObject.feature = "Fairfax County Library";
         updatedObject.streetNumber = data.searchResults.Libraries.STREET_NUMBER;
         updatedObject.address = data.searchResults.Libraries.STREET_NAME;
         updatedObject.zip = data.searchResults.Libraries.ZIP;
         updatedObject.phone = data.searchResults.Libraries.ERC_PHONE;
         updatedObject.web = data.searchResults.Libraries.WEB_ADDRESS;
+      }
+      else {
+        updatedObject.name = data.searchResults.COMMUNITY_CENTERS.DESCRIPTION;
+        updatedObject.feature = "Fairfax County Rec-center";
+        updatedObject.streetNumber = data.searchResults.COMMUNITY_CENTERS.STREET_NUMBER;
+        updatedObject.address = data.searchResults.COMMUNITY_CENTERS.STREET_NAME;
+        updatedObject.zip = data.searchResults.COMMUNITY_CENTERS.ZIP;
+        updatedObject.phone = data.searchResults.COMMUNITY_CENTERS.ERC_PHONE;
+        updatedObject.web = data.searchResults.COMMUNITY_CENTERS.WEB_ADDRESS;
       }
 
 
