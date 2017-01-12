d3a2a399c8692b96cfcc483bfb3ae0fa464a18d6
Syed Ali <sali3@cox.net>
2017-01-12 10:02:32 -0500

    Adding doc blocs and refactoring

--- build/js/app.js
+++ build/js/app.js
@@ -450,7 +450,6 @@
 
   var storedItems = JSON.parse(localStorage.getItem("list"));
   storedItems = storedItems || {coordinates:{}};
-  console.log(storedItems);
 
 
   ParkService.$inject = ["$http", "$q"];
@@ -462,11 +461,12 @@
     };
 
     /**
-    * [This function retrieve list of parks based on users geo position]
+    * [This function retrieve list of parks from a specific geolocation]
     * @param  {Object} coordinates object coordinates with two properties: latitude and longitude
     * @return {Promise}             It returns promise object
     */
     function parkList(coordinates){
+
       if (!coordinates ||  !coordinates.latitude || !coordinates.longitude) {
         return $q.reject(new Error("You must provide an object with latitude and longitude properties"));
       }
@@ -480,7 +480,6 @@
         return $q.resolve(storedItems.list);
       }
 
-      console.log("Making ajax call next");
 
       return $http({
         url: "http://www.fairfaxcounty.gov/FFXGISAPI/v1/search",
@@ -496,7 +495,6 @@
         console.log("Getting Park Data via ajax",response.data);
 
         var allPromises = response.data.searchResults.results.map(function parkDetail(each){
-          console.log("Each location URL",each.url);
           return locationDetail(each.url);
         });
         return $q.all(allPromises);
@@ -511,8 +509,8 @@
 
     /**
     * This function retrives detail of each location.
-    * @param  {[type]} parkUrl [description]
-    * @return {[type]}         [description]
+    * @param  {Object} parkUrl [URL for each park location]
+    * @return {Object}         [It returns a promise object]
     */
     function locationDetail(parkUrl){
       return $http({
@@ -520,7 +518,6 @@
         method: "GET",
       })
       .then(function parkSuccessHandeler(response){
-        console.log("Log me please",response.data);
         return response.data;
       })
       .catch(function parkFailureHandeler(xhr){
@@ -531,18 +528,17 @@
 
 
     /**
-     * This function stores park locations to local storage
-     * @param  {Object} list        [Object containing array of locations list]
-     * @param  {Object} coordinates [Coordinates of user's current location]
-     * @return {Void}
-     */
+    * This function stores park locations to local storage
+    * @param  {Object} list        [Object containing array of locations list]
+    * @param  {Object} coordinates [Coordinates of user's current location]
+    * @return {Void}
+    */
     function updateLocalStorage(list, coordinates){
 
       var data = {
         list: list,
         coordinates: {latitude: coordinates.latitude, longitude: coordinates.longitude}
       };
-      console.log("Saving list of parks to localStorage", data);
       localStorage.setItem("list", angular.toJson(data));
     }
   }
